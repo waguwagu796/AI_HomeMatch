33fc9d7619380db167bfa7188073a24d54d3017e
waguwagu796 <bbosa1012@gmail.com>
2026-02-05 12:12:33 +0900

    123

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react'
+import { useState, type ReactNode } from 'react'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import FloatingChatbot from './FloatingChatbot'
